aebcdb84eb11896726d27650164377def4d713e0
njagih <njagih600@gmail.com>
2026-03-26 10:02:48 +0300

    js login attemp lab

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
+function createLoginTracker(userInfo) {
+  
+  let attemptCount = 0;
+  
+  return (passwordAttempt) => {
+    
+    if (attemptCount >= 3) {
+      return "Account locked due to too many failed login attempts";
+    }
 
+    if (passwordAttempt === userInfo.password) {
+      return "Login successful";
+    } 
+    
+    attemptCount++;
+
+    if(attemptCount <= 3){
+      return `Attempt ${attemptCount}: Login failed`;
+    }
+    return "Account locked due to too many failed login attempts"
+  };
+}
 
 
 module.exports = {
